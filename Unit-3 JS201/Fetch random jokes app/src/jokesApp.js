54c7c8cc7f4014ccb731377aaae272f3a819ba3a
Abhishek <abhisingh24082002@gmail.com>
2022-09-17 23:09:29 +0530

    added fetch random jokes app

---
+++ Unit-3 JS201/Fetch random jokes app/src/jokesApp.js
@@ -0,0 +1,67 @@
+function setJokes(data) {
+  // get the joke-container div
+  let container = document.getElementById("joke-container");
+
+  // craete a div and add a p tag in it with class as joke-text
+  let div = document.createElement("div");
+  let p = document.createElement("p");
+  p.className = "joke-text";
+
+  p.innerHTML = data.joke;
+
+  div.append(p);
+  container.append(div);
+
+  // append the div to joke-container div
+}
+
+let getRandomJoke = async () => {
+  // fetch request to get a random joke
+
+  try {
+    let res = await fetch(`https://v2.jokeapi.dev/joke/Any`);
+    let data = await res.json();
+    // console.log(data);
+    // return the data ona successfull request
+    // console.log(data.joke);
+    setJokes(data);
+    // return data;
+  } catch (err) {
+    // return error text on failure
+    console.log(err);
+  }
+};
+let getJokeByCategory = async (event) => {
+  // // fetch request to get a random joke by category
+
+  try {
+    let res = await fetch(
+      `https://v2.jokeapi.dev/joke/Any?blacklistFlags=${event}`
+    );
+    let result = await res.json();
+    setJokes(result);
+    // return result;
+  } catch (err) {
+    console.log("err", err);
+  }
+  // return the data ona successfull request
+  // return error text on failure
+};
+window.onload = function () {
+  // add click event to button
+  let btn = document.getElementById("get-jokes-data");
+  btn.addEventListener("click", getRandomJoke);
+  // add change event to select tag
+  let categories = document.getElementById("get-category");
+  categories.addEventListener("change", getJokeByCategory);
+};
+
+// donot chnage the export statement
+
+if (typeof exports !== "undefined") {
+  module.exports = {
+    getRandomJoke,
+    setJokes,
+    getJokeByCategory,
+  };
+}
